6787122268654e072c44b0c0d6e98f54b040290e
ka-muraoka <ka-muraoka@ac-tec.co.jp>
2026-03-09 14:51:08 +0900

    ヘッダー機能追加（報告画面にもヘッダー取り込み）

---
+++ src/main/java/com/example/tardiness_report/controller/HeaderController.java
@@ -0,0 +1,40 @@
+package com.example.tardiness_report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HeaderController {
+    /**
+     * メニューへ戻るボタン押下時
+     * 
+     * @param model セッション情報を格納するmodel
+     * @param session セッション情報
+     * @return メニュー画面
+     */
+    @GetMapping("/menuReturn")
+    public String menuReturn(Model model, HttpSession session) {
+
+        // メニュー画面へ遷移
+        return "menu";
+    }
+
+    /**
+     * ログアウトボタン押下時
+     *
+     * @param session セッション情報
+     * @return メニュー画面
+     */
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+
+        // セッションをクリアする
+        session.invalidate();
+
+        // ログイン画面へ遷移
+        return "login";
+    }
+}
